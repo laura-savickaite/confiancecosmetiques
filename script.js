2c247ae269e28a9ae69f5e5a2237b4ab8969c073
Boris <boris.tikhomiroff@lapleforme.io>
2022-03-30 08:06:36 +0200

    add script

--- script.js
+++ script.js
@@ -76,4 +76,54 @@ document.addEventListener('DOMContentLoaded', function loaded() {
         y: "-100%",
         ease: Expo.easeInOut,
     })
+
+
+    //INSTAFEED
+
+    const accessToken = "IGQVJYRUM3UzduM1F6eEdCZAlJQbWpuN0RETDVBQnd1a1lkekhJWVFXa2dNWklXLWtiOXdtTkl0MlRMOVZAtQXFMRnBFR2ljdGVxZATVSRGZAzWTFXUm40YnhIaWN2bF9fc1JSX2UxQnoyWjVuNjBPTmFWaAZDZD";
+
+    const fields = "id,media_type,media_url,timestamp,permalink";
+
+    const accessUrl = `https://graph.instagram.com/me/media?fields=${fields}&access_token=${accessToken}`;
+
+    const section = document.getElementsByClassName("instafeed")
+    const instafeed = section[0];
+    
+    const fetchPosts = async () => {
+
+        try {
+
+            const response = await fetch(accessUrl)
+            const {data} = await response.json()
+
+            data.forEach(post => {
+                let a = document.createElement("a");
+                a.href = post.permalink
+                a.target = "_blank"
+                a.rel = "noreferrer noopener"
+
+                if(post.media_type === "VIDEO"){
+                    let video = document.createElement("video");
+                    video.src = post.media_url
+                    video.preload = true
+                    video.autoplay = true
+                    video.muted = true
+                    video.loop = true
+                    a.appendChild(video)
+                } else {
+                    let img = document.createElement("img");
+                    img.src = post.media_url
+                    a.appendChild(img)
+                }
+                
+                instafeed.appendChild(a)
+
+            });
+
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    fetchPosts();
 })
